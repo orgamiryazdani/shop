8ce28502e3f884d0acc21392b95d1cd4a83b2ec1
amir <orgamiryazdani@gmail.com>
2024-02-13 18:40:44 +0330

    Add Persian translation for new strings, add logout functionality, add confirmation modal for logout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,11 +8,40 @@ import { IoCartOutline } from "react-icons/io5";
 import { useCart } from "@/context/CartContext";
 import { useLanguage } from "@/context/LanguageContext";
 import { toPersianNumbers } from "@/utils/toPersianNumbers";
+import Modal from "@/common/Modal";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import toast from "react-hot-toast";
+import useGetUser from "@/hooks/useAuth";
 
 function Menu() {
+    const [openModal, setOpenModal] = useState(false)
     const pathname = usePathname();
     const { cart } = useCart()
     const { language } = useLanguage()
+    const { t } = useTranslation();
+    const { data } = useGetUser()
+
+    const onClose = () => {
+        setOpenModal(false);
+    }
+
+    const logOutHandler = () => {
+        if (!data?.data) {
+            toast(t('pleaseLogin'));
+            return;
+        } else {
+            setOpenModal(true)
+        }
+    }
+
+    const deleteTokens = () => {
+        document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        toast.success(t('logOut'));
+        onClose();
+        window.location.reload()
+    }
 
     return (
         <div className="md:min-w-16 md:w-16 w-full md:h-72 h-full bg-secondary-200 md:rounded-3xl rounded-t-3xl flex md:flex-col justify-around items-center">
@@ -29,9 +58,19 @@ function Menu() {
                     : null
                 }
             </Link>
-            <button className="menu__item hover:text-red-500">
+            <button onClick={() => logOutHandler()} className="menu__item hover:text-red-500">
                 <TbLogout />
             </button>
+            <Modal
+                open={openModal}
+                onClose={onClose}
+                title={t('modal.logOutTitle')}
+            >
+                <div className="w-full flex items-center justify-around">
+                    <button className="btn btn--primary w-56 mx-1" onClick={() => deleteTokens()}>{t('modal.accBtn')}</button>
+                    <button className="btn btn--primary w-56 mx-1 bg-red-500" onClick={() => onClose()}>{t('modal.closeBtn')}</button>
+                </div>
+            </Modal>
         </div>
     )
 }
