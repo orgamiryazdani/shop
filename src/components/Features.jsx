59c06908af4c10f8b6786f5644aacb7ef5fe3b36
Amir Yazdani <100785957+orgamiryazdani@users.noreply.github.com>
2024-04-02 11:58:33 +0330

    Update Features.jsx linkedin link

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -73,7 +73,7 @@ function Features() {
                 <a href="mailto:orgamiryazdani@gmail.com" target="_blank">
                   <img src="https://img.shields.io/static/v1?message=Gmail&logo=gmail&label=&color=D14836&logoColor=white&labelColor=&style=for-the-badge" height="36" alt="gmail logo" />
                 </a>
-                <a href="https://www.linkedin.com/in/amir-yazdani-org" target="_blank">
+                <a href="https://www.linkedin.com/in/amiryazdanii" target="_blank">
                   <img src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge" height="36" alt="linkedin logo" />
                 </a>
                 <a href="https://t.me/amiiiirryz" target="_blank">
